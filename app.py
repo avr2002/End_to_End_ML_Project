c458ba6366e57b2e91f56c77bcbd0cd1f8ae696f
avr2002 <avr13405@gmail.com>
2022-09-08 06:55:35 +0530

    commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-    return 'Starting Machine Learning Project!\nCI/CD Pipeline Established!'
+    return 'Starting Machine Learning Project!'
 
 
 if __name__ == '__main__':
